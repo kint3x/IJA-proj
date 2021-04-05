dbec1ab2be538a27a5099a9b36c03eca7ee4a6b7
Simon Košina <simonkosina@gmail.com>
2021-04-05 11:40:13 +0200

    metoda importShelfs

--- src/design/Main.java
+++ src/design/Main.java
@@ -1,5 +1,6 @@
 package design;
 
+import design.model.Storage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,13 +11,23 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("design/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
+        Storage storage = new Storage();
+
+        try {
+            storage.importShelfs("input/shelfs.json");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        storage.printShelfs();
+
         launch(args);
     }
 }
--- src/design/model/Storage.java
+++ src/design/model/Storage.java
@@ -1,6 +1,12 @@
 package design.model;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Trieda reprezentujúca sklad.
@@ -43,6 +49,35 @@ public class Storage {
                 Shelf shelf = new Shelf(i,j);
                 this.addShelf(shelf);
             }
+        }
+    }
+
+    /**
+     * Parsuje uvedený súbor, volá metodu createShelfs pre špecifikované rozmery regálov.
+     * @param filename JSON súbor obsahujúci popis regálov
+     * @throws Exception otvorenie a parsovanie súboru
+     */
+    public void importShelfs(String filename) throws Exception {
+        Object obj = new JSONParser().parse(new FileReader(filename));
+
+        JSONObject jo = (JSONObject) obj;
+        JSONArray ja = (JSONArray) jo.get("shelfs");
+
+        for (Object o : ja) {
+            Map m = (Map) o;
+
+            int x1 = ((Long) m.get("x1")).intValue();
+            int y1 = ((Long) m.get("y1")).intValue();
+            int x2 = ((Long) m.get("x2")).intValue();
+            int y2 = ((Long) m.get("y2")).intValue();
+
+            this.createShelfs(x1, y1, x2, y2);
+        }
+    }
+
+    public void printShelfs() {
+        for (Shelf s : this.shelfs) {
+            System.out.format("%d, %d\n", s.getPosX(), s.getPosY());
         }
     }
 }
