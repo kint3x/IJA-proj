4fb22fa0520ae7b53683f3a3eec914cc4a9171e3
Kebab <martin@matejka.xyz>
2021-05-07 12:09:44 +0200

    oprava pre linux

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-wget http://www.stud.fit.vutbr.cz/~xmatej55/IJA/json-simple-1.1.zip
-
+wget http://www.stud.fit.vutbr.cz/~xmatej55/IJA/json-simple-1.1.zip 
 unzip json-simple-1.1.zip
-
 rm -rf json-simple-1.1.zip
